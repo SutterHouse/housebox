5c0c2b32fd1371d117e1880e36c13532d8b088ff
lhmun <louis.mun@gmail.com>
2018-06-08 16:54:52 -0700

    Set up basic components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {
+  Component
+} from 'react';
+import './style.css';
+import Messages from './MessageList.js';
+
+let sampleData = [{
+    user: 'testuser1',
+    message: 'hello!'
+  },
+  {
+    user: 'testuser2',
+    message: 'hey?'
+  }
+];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({messages: sampleData});
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    return ( 
+    <div>
+      <MessageList messages = {this.props.messages}/> </div>
     );
   }
 }
---
+++ src/Message.js
@@ -0,0 +1,15 @@
+import React, {
+  Component
+} from 'react';
+import './style.css';
+
+const Message = (props) => (
+  <div>
+    <div className='message-container'>
+      Message container
+    </div>
+  </div>
+)
+
+
+export default Message;
